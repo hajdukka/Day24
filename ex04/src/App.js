cc9d338a95ed78d67021f90941855838730103bf
hajdukka <nerminhajdo@gmail.com>
2021-04-01 07:08:54 +0200

    ex04

--- ex04/src/App.js
+++ ex04/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <input type="button" value="Set" onClick={this.setLocalStorage} />
+        <input type="button" value="Get" onClick={this.getLocalStorage} />
+      </div>
+    );
+  }
+  setLocalStorage() {
+    localStorage.setItem("Arena", "Selection Month");
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  getLocalStorage() {
+    var myLocalStorageData;
+    myLocalStorageData = localStorage.getItem("Arena");
+  }
 }
 
 export default App;
